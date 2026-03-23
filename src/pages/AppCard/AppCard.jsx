b93007b19e8abae170903c2fb0d5bb163a486281
Nazrul Islam <dobon.islam@icloud.com>
2026-03-23 09:41:01 -0400

    recharts is done

--- src/pages/AppCard/AppCard.jsx
+++ src/pages/AppCard/AppCard.jsx
@@ -2,10 +2,10 @@ import { FaStar } from "react-icons/fa";
 import { FaDownload } from "react-icons/fa";
 import { Link } from "react-router";
 const AppCard = ({ singleApp }) => {
-    const { image, companyName,id, description, ratingAvg, downloads } = singleApp;
+    const { image, companyName, id, description, ratingAvg, downloads } = singleApp;
     console.log(singleApp);
     return (
-        <Link to ={`/appDetails/${id}`}>
+        <Link to={`/appDetails/${id}`}>
             <div className="card bg-base-100 shadow-sm">
                 <figure>
                     <img
@@ -14,7 +14,13 @@ const AppCard = ({ singleApp }) => {
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">{companyName}</h2>
-                    <p>{description}</p>
+
+                    <p>
+                        {description.length > 40
+                            ? description.slice(0, 40) + "..."
+                            : description}
+                    </p>
+
                     <div className="card-actions justify-between">
                         <button className="btn btn-default text-[#00D390] bg-[#F1F5E8]"><FaDownload />{downloads}</button>
                         <button className="btn btn-default text-[#FF8811] bg-[#FFF0E1]"><FaStar />{ratingAvg}</button>
--- src/pages/AppDetails/AppDetails.jsx
+++ src/pages/AppDetails/AppDetails.jsx
@@ -1,14 +1,13 @@
 import { useLoaderData, useParams } from "react-router";
 import RatingsChart from "../../components/RatingsChart/RatingsChart";
+import { FaDownload, FaStar } from "react-icons/fa";
 
 const AppDetails = () => {
     const appsData = useLoaderData();
     const { id } = useParams();
 
-    // ✅ DEFINE app
     const app = appsData.find(item => item.id === parseInt(id));
 
-    // ✅ safety check
     if (!app) {
         return <h1>App not found</h1>;
     }
@@ -16,24 +15,31 @@ const AppDetails = () => {
     return (
         <div className="my-6 py-4">
 
-
-            <div className="md:flex gap-4 ">
-                <div>
-                    <img className="w-80 h-80" src={app.image} alt="" />
+            <div className="flex flex-col md:flex-row gap-4">
+                  <div className="flex justify-center md:justify-start">       
+                    <img className="w-60 h-60 md:w-80 md:h-80 object-cover" src={app.image} alt="" />
                 </div>
-                <div>
+                <div className="space-y-4 text-center md:text-left">                  
                     <h1 className="text-2xl font-bold">{app.title}</h1>
-                    <p>{app.companyName}</p>
-                    <p>{app.description}</p>
-
+                    <p>{app.companyName}</p>      
+                   <div className="card-actions justify-center md:justify-start gap-2">
+                        <button className="btn btn-default text-[#00D390] bg-[#F1F5E8]">
+                            <FaDownload />{app.downloads}
+                        </button>
+                      <button className="btn btn-default text-[#FF8811] bg-[#FFF0E1]">
+                            <FaStar />{app.ratingAvg}
+                        </button>
+                    </div>
                 </div>
 
             </div>
-            <div className="max-w-4xl">
-                {/* ✅ Chart */}
-                <RatingsChart ratings={app.ratings} />
 
+            <div className="max-w-4xl mb-8 ">
+                <RatingsChart ratings={app.ratings} />
             </div>
+
+         <p className="mx-4 md:mx-0">{app.description}</p>
+
         </div>
     );
 };
