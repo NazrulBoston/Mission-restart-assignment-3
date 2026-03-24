99f140405fe3db97fa878db6346eaf2dcdba0d42
Nazrul Islam <dobon.islam@icloud.com>
2026-03-24 11:59:45 -0400

    Bug fixed and apps page done

--- src/pages/Apps/Apps.jsx
+++ src/pages/Apps/Apps.jsx
@@ -6,7 +6,7 @@ const Apps = () => {
     console.log(appsData);
     return (
         <div>
-            <h1>This is apps page</h1>
+           
           <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
               {
                 appsData.map(singleApp=> <AppCard key={singleApp.id} singleApp={singleApp}></AppCard>)
